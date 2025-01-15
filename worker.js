0cc8e6a8d070250bb028b848d981407c3f8991d3
“softontechsolution” <softontechsolution@gmail.com>
2025-01-15 04:58:31 -0800

    worker file

--- worker.js
+++ worker.js
@@ -1,73 +1,81 @@
-import Queue from 'bull';
-import { ObjectId } from 'mongodb';
-import { promises as fsPromises } from 'fs';
-import fileUtils from './utils/file';
-import userUtils from './utils/user';
-import basicUtils from './utils/basic';
+/* eslint-disable import/no-named-as-default */
+import { writeFile } from 'fs';
+import { promisify } from 'util';
+import Queue from 'bull/lib/queue';
+import imgThumbnail from 'image-thumbnail';
+import mongoDBCore from 'mongodb/lib/core';
+import dbClient from './utils/db';
+import Mailer from './utils/mailer';
 
-const imageThumbnail = require('image-thumbnail');
+const writeFileAsync = promisify(writeFile);
+const fileQueue = new Queue('thumbnail generation');
+const userQueue = new Queue('email sending');
 
-const fileQueue = new Queue('fileQueue');
-const userQueue = new Queue('userQueue');
+/**
+ * Generates the thumbnail of an image with a given width size.
+ * @param {String} filePath The location of the original file.
+ * @param {number} size The width of the thumbnail.
+ * @returns {Promise<void>}
+ */
+const generateThumbnail = async (filePath, size) => {
+  const buffer = await imgThumbnail(filePath, { width: size });
+  console.log(`Generating file: ${filePath}, size: ${size}`);
+  return writeFileAsync(`${filePath}_${size}`, buffer);
+};
 
-fileQueue.process(async (job) => {
-  const { fileId, userId } = job.data;
-
-  // Delete bull keys in redis
-  //   redis-cli keys "bull*" | xargs redis-cli del
-
-  if (!userId) {
-    console.log('Missing userId');
-    throw new Error('Missing userId');
-  }
+fileQueue.process(async (job, done) => {
+  const fileId = job.data.fileId || null;
+  const userId = job.data.userId || null;
 
   if (!fileId) {
-    console.log('Missing fileId');
     throw new Error('Missing fileId');
   }
-
-  if (!basicUtils.isValidId(fileId) || !basicUtils.isValidId(userId)) throw new Error('File not found');
-
-  const file = await fileUtils.getFile({
-    _id: ObjectId(fileId),
-    userId: ObjectId(userId),
-  });
-
-  if (!file) throw new Error('File not found');
-
-  const { localPath } = file;
-  const options = {};
-  const widths = [500, 250, 100];
-
-  widths.forEach(async (width) => {
-    options.width = width;
-    try {
-      const thumbnail = await imageThumbnail(localPath, options);
-      await fsPromises.writeFile(`${localPath}_${width}`, thumbnail);
-      //   console.log(thumbnail);
-    } catch (err) {
-      console.error(err.message);
-    }
-  });
-});
-
-userQueue.process(async (job) => {
-  const { userId } = job.data;
-  // Delete bull keys in redis
-  //   redis-cli keys "bull*" | xargs redis-cli del
-
   if (!userId) {
-    console.log('Missing userId');
     throw new Error('Missing userId');
   }
+  console.log('Processing', job.data.name || '');
+  const file = await (await dbClient.filesCollection())
+    .findOne({
+      _id: new mongoDBCore.BSON.ObjectId(fileId),
+      userId: new mongoDBCore.BSON.ObjectId(userId),
+    });
+  if (!file) {
+    throw new Error('File not found');
+  }
+  const sizes = [500, 250, 100];
+  Promise.all(sizes.map((size) => generateThumbnail(file.localPath, size)))
+    .then(() => {
+      done();
+    });
+});
 
-  if (!basicUtils.isValidId(userId)) throw new Error('User not found');
+userQueue.process(async (job, done) => {
+  const userId = job.data.userId || null;
 
-  const user = await userUtils.getUser({
-    _id: ObjectId(userId),
-  });
-
-  if (!user) throw new Error('User not found');
-
+  if (!userId) {
+    throw new Error('Missing userId');
+  }
+  const user = await (await dbClient.usersCollection())
+    .findOne({ _id: new mongoDBCore.BSON.ObjectId(userId) });
+  if (!user) {
+    throw new Error('User not found');
+  }
   console.log(`Welcome ${user.email}!`);
+  try {
+    const mailSubject = 'Welcome to ALX-Files_Manager by B3zaleel';
+    const mailContent = [
+      '<div>',
+      '<h3>Hello {{user.name}},</h3>',
+      'Welcome to <a href="https://github.com/B3zaleel/alx-files_manager">',
+      'ALX-Files_Manager</a>, ',
+      'a simple file management API built with Node.js by ',
+      '<a href="https://github.com/B3zaleel">Bezaleel Olakunori</a>. ',
+      'We hope it meets your needs.',
+      '</div>',
+    ].join('');
+    Mailer.sendMail(Mailer.buildMessage(user.email, mailSubject, mailContent));
+    done();
+  } catch (err) {
+    done(err);
+  }
 });
